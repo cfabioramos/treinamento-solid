2ecd4cb773f1c8e46cf4a86673c601f3c6f1c8c5
Carlos Fabio <cfabioramos@gmail.com>
2018-11-06 16:42:39 -0200

    Codigo do exercicio

--- EstudoSolid/src/com/br/srp/exercicio/solucaoSugerida/Principal.java
+++ EstudoSolid/src/com/br/srp/exercicio/solucaoSugerida/Principal.java
@@ -1,10 +1,17 @@
 package com.br.srp.exercicio.solucaoSugerida;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Principal {
 
 	public static void main(String[] args) {
-    	GeradorDeNotaFiscal gerador = new GeradorDeNotaFiscal(
-    			new EnviadorDeEmail(), new NotaFiscalDao());
+		
+		List<AcaoPosGeracaoNota> acoesPos = new ArrayList<>();
+		acoesPos.add(new EnviadorDeEmail());
+		acoesPos.add(new NotaFiscalDao());
+		
+    	GeradorDeNotaFiscal gerador = new GeradorDeNotaFiscal(acoesPos);
     	
     	NotaFiscal nf = gerador.gera(new Fatura(24.00));
     	
